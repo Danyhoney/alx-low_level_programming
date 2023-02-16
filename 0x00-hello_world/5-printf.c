e384e14e7d16f0c09c9161b1d46d3ffbe1db1d7d
Danyhoney <danielnyong4faith@gmail.com>
2023-02-16 12:08:33 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
