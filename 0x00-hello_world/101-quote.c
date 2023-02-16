92c27071f9a4c977f0c5dad3c860cebc9dac0e7d
Danyhoney <danielnyong4faith@gmail.com>
2023-02-16 12:43:34 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that print a line to the standard error
+ * Return 1 (Succes)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+{
