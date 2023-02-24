b9c32d10325585c5bfe149b8516a3ebaaf00dc02
Danyhoney <danielnyong4faith@gmail.com>
2023-02-23 22:27:03 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+
+* main - Test function for positive or negative
+
+* Return: 0
+
+*/
+
+int main(void)
+
+{
+
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
+}
