9ee4edc2c373e87b826a5f447a91fd6898dcaac4
Danyhoney <danielnyong4faith@gmail.com>
2023-02-24 09:17:05 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,21 +1,27 @@
 #include "main.h"
-
 /**
  * main - check the code
- *
+
  * Return: Always 0.
  */
+
 int main(void)
+	
 {
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
+	print_number(98);
+	_putchar('\n');
+
+	print_number(402);
+	_putchar('\n');
+
+	print_number(1024);
+	_putchar('\n');
+
+	print_number(0);
+	_putchar('\n');
+
+	print_number(-98);
+	_putchar('\n');
+
+	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,26 @@
 #include "main.h"
-/**
- * print_number - prints an integer
- * @n: integer to be printed
- */
+#include <stdio.h>
 
+/**
+ * print_number - prints an integer.
+ * @n: The integer to prints.
+ * Return: Nothing!
+ /*
+ *
 void print_number(int n)
 
 {
 
-unsigned int n1;
+	unsigned int k = n;
 
-
-if (n < 0)
-
-{
-
-n1 = -n;
-
-_putchar('-');
-
-}
-
-else
-
-{
-
-n1 = n;
-
-}
-
-
-if (n1 / 10)
-
-{
-
-print_number(n1 / 10);
-
-}
-
-
-_putchar((n1 % 10) + '0');
-
+	if (n < 0)
+	{
+	n *= -1;
+	k = n;
+	_putchar('_');
+	{
+	k /= 10;
+	if (k != 0)
+	print_number(k);
+	_putchar((unsigned int) n % 10 + '0';
 }
