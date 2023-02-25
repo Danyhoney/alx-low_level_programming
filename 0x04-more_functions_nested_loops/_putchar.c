62ba9e534730933b04dcb75fc1b42b42a7eff5ac
Danyhoney <danielnyong4faith@gmail.com>
2023-02-25 06:35:42 -0800

    _putchar.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <unistd.h>
+#include "holberton.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,4 +11,4 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-
+}
